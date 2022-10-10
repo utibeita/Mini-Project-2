891ed8bdc42c2575dd2d950c605f194a7d820e4a
utibeita <itautibeabasi@gmail.com>
2022-10-10 05:27:38 +0100

    Final Commit

--- app/src/main/java/com/example/miniproject2/SampleData.kt
+++ app/src/main/java/com/example/miniproject2/SampleData.kt
@@ -3,15 +3,15 @@ package com.example.miniproject2
 class SampleData {
     val LIST_OF_FRIENDS: List<Friends> = listOf(
         Friends("My Gee", "Hi i am working"),
-        Friends("Sucre", "Hey i am using whatsapp"),
+        Friends("Sucre", "Hey there i am using whatsapp"),
         Friends("Ukeme Ita", "Programming vibes"),
         Friends("Joan", "I love coding"),
         Friends("Honey Bee", "Hey i am using whatsapp"),
-        Friends("My Gee", "Hi i am working"),
-        Friends("Sucre", "Hey i am using whatsapp"),
-        Friends("Ukeme Ita", "Programming vibes"),
-        Friends("Joan", "I love coding"),
-        Friends("Honey Bee", "Hey i am using whatsapp")
+        Friends("Emem Ita", "Hi i am at work"),
+        Friends("Chidera Otu", "Hey i am online"),
+        Friends("Chinonso Felix", "Away"),
+        Friends("Joy Fred", "Nice to meet you"),
+        Friends("Goody Boo", "Lonely")
 
         )
 }
